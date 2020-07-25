b7c4352d925bffbc66fc2a78ae5ac1a9ce6b46d5
Ponomarev Stepan <htconesuper11@gmail.com>
2020-07-25 13:02:36 +0300

    Aded joining to voice channel function

--- src/main/java/bot/listeners/YaMusicCommandsListener.java
+++ src/main/java/bot/listeners/YaMusicCommandsListener.java
@@ -7,6 +7,9 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Timer;
 
 public final class YaMusicCommandsListener extends ListenerAdapter {
     private final ICommandHandler commandHandler;
@@ -29,17 +32,17 @@ public final class YaMusicCommandsListener extends ListenerAdapter {
         } catch (Exception exception) {
             event.getMessage()
                     .getTextChannel()
-                    .sendMessage("Unknown Error!").submit();
+                    .sendMessage("Command is failed.").submit();
         }
     }
 
-    private void execute(final Command command, final GuildMessageReceivedEvent event) {
+    private void execute(final Command command, final GuildMessageReceivedEvent event) throws Exception {
         switch (command) {
             case HELP:
                 event.getMessage().getTextChannel().sendMessage("HELP DOESN'T WORK").submit();
                 return;
             case PLAY_SONG:
-                event.getMessage().getTextChannel().sendMessage("PLAY DOESN'T WORK").submit();
+                handlePlaySong(event);
                 return;
             case SKIP_SONG:
                 event.getMessage().getTextChannel().sendMessage("SKIP DOESN'T WORK").submit();
@@ -47,5 +50,29 @@ public final class YaMusicCommandsListener extends ListenerAdapter {
             default:
                 return;
         }
+    }
+
+    private void handlePlaySong(final GuildMessageReceivedEvent event) throws MalformedURLException {
+        final var strippetCommand = event.getMessage().getContentStripped().split(" ");
+        final var url = new URL(strippetCommand[1]);
+
+        final var voiceChannel = event.getMember().getVoiceState().getChannel();
+
+        if (voiceChannel == null) {
+            event.getMessage().getTextChannel().sendMessage("You must be in voice channel").submit();
+            return;
+        }
+
+        final var audioManager = event.getMember().getGuild().getAudioManager();
+
+        audioManager.openAudioConnection(voiceChannel);
+        //TODO: https://github.com/DV8FromTheWorld/JDA/wiki/4%29-Making-a-Music-Bot#Sending-Audio-to-an-Open-Audio-Connection
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        audioManager.closeAudioConnection();
     }
 }
