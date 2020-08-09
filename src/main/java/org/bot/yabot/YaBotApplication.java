4883fa0ac4bb51d720fafd6e7d8db98f988bb2b8
Ponomarev Stepan <htconesuper11@gmail.com>
2020-08-10 01:02:48 +0300

    Added env

--- src/main/java/org/bot/yabot/YaBotApplication.java
+++ src/main/java/org/bot/yabot/YaBotApplication.java
@@ -7,10 +7,15 @@ import org.bot.yabot.consts.Consts;
 
 public final class YaBotApplication {
   static final Dotenv dotenv = Dotenv.load();
+  private static final boolean dev = false;
 
   public static void main(String[] args) {
     try {
-      var bot = BotFactory.getInstance(dotenv.get(Consts.BOT_TOKEN));
+      var TOKEN = dev ?
+          dotenv.get(Consts.BOT_TOKEN)
+          : System.getenv(Consts.BOT_TOKEN);
+
+      var bot = BotFactory.getInstance(TOKEN);
     } catch (Exception e) {
       e.printStackTrace();
     }
