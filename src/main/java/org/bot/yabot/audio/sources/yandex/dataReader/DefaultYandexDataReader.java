8a793b18ea24f50fbfd2154f530991ea37b6f5dc
Ponomarev Stepan <htconesuper11@gmail.com>
2020-08-11 22:18:29 +0300

    Fixed AudioTrackInfo

--- src/main/java/org/bot/yabot/audio/sources/yandex/dataReader/DefaultYandexDataReader.java
+++ src/main/java/org/bot/yabot/audio/sources/yandex/dataReader/DefaultYandexDataReader.java
@@ -3,6 +3,8 @@ package org.bot.yabot.audio.sources.yandex.dataReader;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class DefaultYandexDataReader implements YandexDataReader {
@@ -10,17 +12,16 @@ public class DefaultYandexDataReader implements YandexDataReader {
   @Override
   public AudioTrackInfo createTrackInfo(JSONObject rootData) {
     var track = rootData.optJSONObject("track");
-    var author = rootData.getJSONArray("artists").toList();
+    var author = rootData.getJSONArray("artists");
+
+    final List<String> authorNames = new ArrayList<>();
+    for (int i = 0; i < author.length(); ++i) {
+      authorNames.add(((JSONObject) author.opt(i)).getString("name"));
+    }
 
     final String title = track.getString("title");
-    final String authorName = author.size() != 1 ?
-        author.stream()
-            .map(athor -> ((JSONObject) athor).getString("name"))
-            .collect(Collectors.joining(" & ")) :
-        ((JSONObject) author).getString("name");
+    final String authorName = authorNames.stream().collect(Collectors.joining(" & "));
     final long duration = track.getLong("durationMs");
-
-    System.out.println(authorName);
 
     return new AudioTrackInfo(
         title,
